3ffca52e6a63d8815a6aa9ff661365c51181a17a
dania istanboli <dania.ist.mi96@gmail.com>
2022-11-04 00:36:05 +0200

    grouping components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,8 @@
 export { default as Login } from "./auth/Login";
 export { default as Signup } from "./auth/Signup";
+export { default as SideDrawer } from "./chat/SideDrawer";
+export { default as MyChats } from "./chat/MyChats";
+export { default as ChatBox } from "./chat/ChatBox";
+export { default as ProfileModal } from "./modals/ProfileModal";
+export { default as UserListItem } from "./chat/utility/UserLIstItem";
+export { default as ChatLoading } from "./chat/utility/ChatLoading";
