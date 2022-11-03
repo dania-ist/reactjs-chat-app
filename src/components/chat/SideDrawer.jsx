1ba66eac3c49cd11f47c058631ea51485d9c1e0a
dania istanboli <dania.ist.mi96@gmail.com>
2022-11-04 00:33:55 +0200

    add chat page ui&logic

---
+++ src/components/chat/SideDrawer.jsx
@@ -0,0 +1,176 @@
+import { Button } from "@chakra-ui/button";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Input } from "@chakra-ui/input";
+import { Box, Text } from "@chakra-ui/layout";
+import {
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/menu";
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+} from "@chakra-ui/modal";
+import { Tooltip } from "@chakra-ui/tooltip";
+import { BellIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import { Avatar } from "@chakra-ui/avatar";
+import { useToast } from "@chakra-ui/toast";
+import { Spinner } from "@chakra-ui/spinner";
+
+import { useState } from "react";
+
+import { TheAppState } from "../../context/TheAppProvider";
+import { useNavigate } from "react-router-dom";
+import SearchHook from "../../helper/search/search-hook";
+import { ChatLoading, UserListItem, ProfileModal } from "../index";
+import baseUrl from "../../api/baseURL";
+
+const SideDrawer = () => {
+  const [
+    search,
+    onChangeSearch,
+    setSearch,
+    searchResult,
+    setSearchResult,
+    handleSearch,
+    loading,
+    setLoading,
+  ] = SearchHook();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [loadingChat, setLoadingChat] = useState(false);
+
+  const { setSelectedChat, user, chats, setChats } = TheAppState();
+
+  const navigate = useNavigate();
+  const toast = useToast();
+
+  const logoutHandler = () => {
+    localStorage.removeItem("userInfo");
+    navigate("/");
+  };
+
+  const accessChat = async (userId) => {
+    try {
+      setLoadingChat(true);
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await baseUrl.post(`/api/chat`, { userId }, config);
+
+      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
+      setSelectedChat(data);
+      setLoadingChat(false);
+      setSearch("");
+      setSearchResult([]);
+      onClose();
+    } catch (error) {
+      toast({
+        title: "Error fetching the chat",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "top right",
+      });
+    }
+  };
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="white"
+        w="100%"
+        p="5px 10px 5px 10px"
+        borderWidth="5px"
+      >
+        <Tooltip label="Search Users to chat" hasArrow placement="bottom-end">
+          <Button variant="ghost" onClick={onOpen}>
+            <i className="fas fa-search"></i>
+            <Text d={{ base: "none", md: "flex" }} px={4}>
+              Search User
+            </Text>
+          </Button>
+        </Tooltip>
+        <Text fontSize="2xl" fontFamily="Work sans">
+          chat-application
+        </Text>
+        <div>
+          <Menu>
+            <MenuButton p={1}>
+              <BellIcon fontSize="2xl" m={1} />
+            </MenuButton>
+          </Menu>
+          <Menu>
+            <MenuButton as={Button} bg="white" rightIcon={<ChevronDownIcon />}>
+              <Avatar
+                size="sm"
+                cursor="pointer"
+                name={user?.name}
+                src={user?.imageProfile}
+              />
+            </MenuButton>
+            <MenuList>
+              <ProfileModal user={user && user}>
+                <MenuItem>My Profile</MenuItem>
+              </ProfileModal>
+              <MenuDivider />
+              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </div>
+      </Box>
+
+      <Drawer
+        placement="left"
+        onClose={() => {
+          setSearch("");
+          setSearchResult([]);
+          onClose();
+        }}
+        isOpen={isOpen}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Search Users</DrawerHeader>
+          <DrawerBody>
+            <Box display="flex" pb={2}>
+              <Input
+                placeholder="Search by name or email"
+                mr={2}
+                value={search}
+                onChange={onChangeSearch}
+              />
+              <Button onClick={handleSearch}>Go</Button>
+            </Box>
+            {loading ? (
+              <ChatLoading />
+            ) : (
+              searchResult?.map((usr) => (
+                <UserListItem
+                  key={usr._id}
+                  user={usr}
+                  handleFunction={() => accessChat(usr._id)}
+                />
+              ))
+            )}
+            {loadingChat && <Spinner ml="auto" display="flex" />}
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default SideDrawer;
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Box } from "@chakra-ui/layout";
+import { ChatBox, MyChats, SideDrawer } from "../components";
 
-const Chat = () => {
-  return <div>Chat</div>;
+import { TheAppState } from "../context/TheAppProvider";
+
+const Chats = () => {
+  const { user } = TheAppState();
+
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
+    </div>
+  );
 };
 
-export default Chat;
+export default Chats;
